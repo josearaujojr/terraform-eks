cf92bfc704fe9f1bf3cbc04e77de32a3dcfcd585
Jose Almino <jose.araujo@compasso.com.br>
2024-06-10 11:12:15 -0300

    feat: remote backend

--- provider.tf
+++ provider.tf
@@ -1,12 +1,13 @@
+provider "aws" {
+  region  = "us-east-1"
+  profile = "cli"
+}
+
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.53.0"
     }
   }
-}
-
-provider "aws" {
-    region = "us-east-1"
 }
