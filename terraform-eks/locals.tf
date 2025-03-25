520b480be05f86e77c4c3a4c887259de8526d10d
Jose Almino <jose.almino.jr@gmail.com>
2025-03-25 10:46:30 -0300

    ajuste na estrutura de arquivos

--- terraform-eks/locals.tf
+++
@@ -1,9 +0,0 @@
-locals {
-  tags = {
-    Department   = "DevOps"
-    Organization = "Infrastructure and Operations"
-    Project      = "EKS"
-    Environment  = "Development"
-    "karpenter.sh/discovery" = "app-eks-cluster"
-  }
-}
--- terraform-eks/modules/ecr/ecr.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_ecr_repository" "app" {
-  name                 = "${var.project_name}-app"
-  image_tag_mutability = "MUTABLE"
-  tags = var.tags
-  
-  provisioner "local-exec" {
-    command = "sleep 30"
-  }
-}
