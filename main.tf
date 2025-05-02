963721e07995610e58df7b1ab088589bfbaae554
Jose Almino <jose.almino.jr@gmail.com>
2025-05-02 14:36:23 -0300

    implementação External Secrets Operator

--- main.tf
+++ main.tf
@@ -145,217 +145,235 @@ resource "helm_release" "nginx_ingress" {
 
 }
 
-########################## CSI SECRET STORE
+########################## EXTERNAL SECRETS OPERATOR
 
-# install do Secrets Store CSI Driver usando o Helm Release
-resource "helm_release" "csi_secrets_store" {
-  name       = "csi-secrets-store"
-  namespace  = "kube-system"
-  repository = "https://kubernetes-sigs.github.io/secrets-store-csi-driver/charts"
-  chart      = "secrets-store-csi-driver"
-  version    = "1.3.3"  # version do chart
+resource "helm_release" "external_secrets" {
+  name       = "external-secrets"
+  repository = "https://charts.external-secrets.io"
+  chart      = "external-secrets"
+  version    = "0.9.15"
+  namespace  = "external-secrets"
 
-  set {
-    name  = "syncSecret.enabled"
-    value = "true"
-  }
+  create_namespace = true
 
-  set {
-    name  = "enableSecretRotation"
-    value = "true"
-  }
-
-  set {
-    name  = "rotationPollInterval"
-    value = "10s"
-  }
-
-  # values = [
-  #   file("${path.module}/values.yaml")  # files de valores, opcional
-  # ]
+  values = [
+    yamlencode({
+      installCRDs = true
+    })
+  ]
 }
 
-resource "kubectl_manifest" "csi_secret_0" {
-  yaml_body = <<-EOT
-# https://kubernetes.io/docs/reference/access-authn-authz/rbac
-apiVersion: v1
-kind: ServiceAccount
-metadata:
-  name: csi-secrets-store-provider-aws
-  namespace: kube-system
-  EOT
+# ########################## CSI SECRET STORE
 
-  depends_on = [module.eks_cluster]
-}
+# # install do Secrets Store CSI Driver usando o Helm Release
+# resource "helm_release" "csi_secrets_store" {
+#   name       = "csi-secrets-store"
+#   namespace  = "kube-system"
+#   repository = "https://kubernetes-sigs.github.io/secrets-store-csi-driver/charts"
+#   chart      = "secrets-store-csi-driver"
+#   version    = "1.3.3"  # version do chart
+
+#   set {
+#     name  = "syncSecret.enabled"
+#     value = "true"
+#   }
+
+#   set {
+#     name  = "enableSecretRotation"
+#     value = "true"
+#   }
+
+#   set {
+#     name  = "rotationPollInterval"
+#     value = "10s"
+#   }
+
+#   # values = [
+#   #   file("${path.module}/values.yaml")  # files de valores, opcional
+#   # ]
+# }
+
+# resource "kubectl_manifest" "csi_secret_0" {
+#   yaml_body = <<-EOT
+# # https://kubernetes.io/docs/reference/access-authn-authz/rbac
+# apiVersion: v1
+# kind: ServiceAccount
+# metadata:
+#   name: csi-secrets-store-provider-aws
+#   namespace: kube-system
+#   EOT
+
+#   depends_on = [module.eks_cluster]
+# }
 
 
-resource "kubectl_manifest" "csi_secret_1" {
-  yaml_body = <<-EOT
-apiVersion: rbac.authorization.k8s.io/v1
-kind: ClusterRole
-metadata:
-  name: csi-secrets-store-provider-aws-cluster-role
-rules:
-- apiGroups: [""]
-  resources: ["serviceaccounts/token"]
-  verbs: ["create"]
-- apiGroups: [""]
-  resources: ["serviceaccounts"]
-  verbs: ["get"]
-- apiGroups: [""]
-  resources: ["pods"]
-  verbs: ["get"]
-- apiGroups: [""]
-  resources: ["nodes"]
-  verbs: ["get"]
-  EOT
+# resource "kubectl_manifest" "csi_secret_1" {
+#   yaml_body = <<-EOT
+# apiVersion: rbac.authorization.k8s.io/v1
+# kind: ClusterRole
+# metadata:
+#   name: csi-secrets-store-provider-aws-cluster-role
+# rules:
+# - apiGroups: [""]
+#   resources: ["serviceaccounts/token"]
+#   verbs: ["create"]
+# - apiGroups: [""]
+#   resources: ["serviceaccounts"]
+#   verbs: ["get"]
+# - apiGroups: [""]
+#   resources: ["pods"]
+#   verbs: ["get"]
+# - apiGroups: [""]
+#   resources: ["nodes"]
+#   verbs: ["get"]
+#   EOT
 
-  depends_on = [module.eks_cluster]
-}
+#   depends_on = [module.eks_cluster]
+# }
 
-resource "kubectl_manifest" "csi_secret_2" {
-  yaml_body = <<-EOT
-apiVersion: rbac.authorization.k8s.io/v1
-kind: ClusterRoleBinding
-metadata:
-  name: csi-secrets-store-provider-aws-cluster-rolebinding
-roleRef:
-  apiGroup: rbac.authorization.k8s.io
-  kind: ClusterRole
-  name: csi-secrets-store-provider-aws-cluster-role
-subjects:
-- kind: ServiceAccount
-  name: csi-secrets-store-provider-aws
-  namespace: kube-system
-  EOT
+# resource "kubectl_manifest" "csi_secret_2" {
+#   yaml_body = <<-EOT
+# apiVersion: rbac.authorization.k8s.io/v1
+# kind: ClusterRoleBinding
+# metadata:
+#   name: csi-secrets-store-provider-aws-cluster-rolebinding
+# roleRef:
+#   apiGroup: rbac.authorization.k8s.io
+#   kind: ClusterRole
+#   name: csi-secrets-store-provider-aws-cluster-role
+# subjects:
+# - kind: ServiceAccount
+#   name: csi-secrets-store-provider-aws
+#   namespace: kube-system
+#   EOT
 
-  depends_on = [module.eks_cluster]
-}
+#   depends_on = [module.eks_cluster]
+# }
 
-resource "kubectl_manifest" "csi_secret_3" {
-  yaml_body = <<-EOT
-apiVersion: apps/v1
-kind: DaemonSet
-metadata:
-  namespace: kube-system
-  name: csi-secrets-store-provider-aws
-  labels:
-    app: csi-secrets-store-provider-aws
-spec:
-  updateStrategy:
-    type: RollingUpdate
-  selector:
-    matchLabels:
-      app: csi-secrets-store-provider-aws
-  template:
-    metadata:
-      labels:
-        app: csi-secrets-store-provider-aws
-    spec:
-      serviceAccountName: csi-secrets-store-provider-aws
-      hostNetwork: false
-      containers:
-        - name: provider-aws-installer
-          image: public.ecr.aws/aws-secrets-manager/secrets-store-csi-driver-provider-aws:1.0.r2-72-gfb78a36-2024.05.29.23.03
-          imagePullPolicy: Always
-          args:
-              - --provider-volume=/etc/kubernetes/secrets-store-csi-providers
-          resources:
-            requests:
-              cpu: 50m
-              memory: 100Mi
-            limits:
-              cpu: 50m
-              memory: 100Mi
-          securityContext:
-            privileged: false
-            allowPrivilegeEscalation: false
-          volumeMounts:
-            - mountPath: "/etc/kubernetes/secrets-store-csi-providers"
-              name: providervol
-            - name: mountpoint-dir
-              mountPath: /var/lib/kubelet/pods
-              mountPropagation: HostToContainer
-      volumes:
-        - name: providervol
-          hostPath:
-            path: "/etc/kubernetes/secrets-store-csi-providers"
-        - name: mountpoint-dir
-          hostPath:
-            path: /var/lib/kubelet/pods
-            type: DirectoryOrCreate
-      nodeSelector:
-        kubernetes.io/os: linux
-  EOT
+# resource "kubectl_manifest" "csi_secret_3" {
+#   yaml_body = <<-EOT
+# apiVersion: apps/v1
+# kind: DaemonSet
+# metadata:
+#   namespace: kube-system
+#   name: csi-secrets-store-provider-aws
+#   labels:
+#     app: csi-secrets-store-provider-aws
+# spec:
+#   updateStrategy:
+#     type: RollingUpdate
+#   selector:
+#     matchLabels:
+#       app: csi-secrets-store-provider-aws
+#   template:
+#     metadata:
+#       labels:
+#         app: csi-secrets-store-provider-aws
+#     spec:
+#       serviceAccountName: csi-secrets-store-provider-aws
+#       hostNetwork: false
+#       containers:
+#         - name: provider-aws-installer
+#           image: public.ecr.aws/aws-secrets-manager/secrets-store-csi-driver-provider-aws:1.0.r2-72-gfb78a36-2024.05.29.23.03
+#           imagePullPolicy: Always
+#           args:
+#               - --provider-volume=/etc/kubernetes/secrets-store-csi-providers
+#           resources:
+#             requests:
+#               cpu: 50m
+#               memory: 100Mi
+#             limits:
+#               cpu: 50m
+#               memory: 100Mi
+#           securityContext:
+#             privileged: false
+#             allowPrivilegeEscalation: false
+#           volumeMounts:
+#             - mountPath: "/etc/kubernetes/secrets-store-csi-providers"
+#               name: providervol
+#             - name: mountpoint-dir
+#               mountPath: /var/lib/kubelet/pods
+#               mountPropagation: HostToContainer
+#       volumes:
+#         - name: providervol
+#           hostPath:
+#             path: "/etc/kubernetes/secrets-store-csi-providers"
+#         - name: mountpoint-dir
+#           hostPath:
+#             path: /var/lib/kubelet/pods
+#             type: DirectoryOrCreate
+#       nodeSelector:
+#         kubernetes.io/os: linux
+#   EOT
 
-  depends_on = [module.eks_cluster]
-}
+#   depends_on = [module.eks_cluster]
+# }
 
-resource "kubectl_manifest" "csi_secret_4" {
-  yaml_body = <<-EOT
-apiVersion: rbac.authorization.k8s.io/v1
-kind: ClusterRole
-metadata:
-  name: secrets-store-csi-driver-cluster-role
-rules:
-- apiGroups: [""]
-  resources: ["secrets"]
-  verbs: ["get", "list", "watch"]
-EOT
+# resource "kubectl_manifest" "csi_secret_4" {
+#   yaml_body = <<-EOT
+# apiVersion: rbac.authorization.k8s.io/v1
+# kind: ClusterRole
+# metadata:
+#   name: secrets-store-csi-driver-cluster-role
+# rules:
+# - apiGroups: [""]
+#   resources: ["secrets"]
+#   verbs: ["get", "list", "watch"]
+# EOT
 
-  depends_on = [module.eks_cluster]
-}
+#   depends_on = [module.eks_cluster]
+# }
 
-resource "kubectl_manifest" "csi_secret_5" {
-  yaml_body = <<-EOT
-apiVersion: rbac.authorization.k8s.io/v1
-kind: ClusterRoleBinding
-metadata:
-  name: secrets-store-csi-driver-cluster-rolebinding
-subjects:
-- kind: ServiceAccount
-  name: secrets-store-csi-driver
-  namespace: kube-system
-roleRef:
-  kind: ClusterRole
-  name: secrets-store-csi-driver-cluster-role
-  apiGroup: rbac.authorization.k8s.io
-EOT
+# resource "kubectl_manifest" "csi_secret_5" {
+#   yaml_body = <<-EOT
+# apiVersion: rbac.authorization.k8s.io/v1
+# kind: ClusterRoleBinding
+# metadata:
+#   name: secrets-store-csi-driver-cluster-rolebinding
+# subjects:
+# - kind: ServiceAccount
+#   name: secrets-store-csi-driver
+#   namespace: kube-system
+# roleRef:
+#   kind: ClusterRole
+#   name: secrets-store-csi-driver-cluster-role
+#   apiGroup: rbac.authorization.k8s.io
+# EOT
 
-  depends_on = [module.eks_cluster]
-}
+#   depends_on = [module.eks_cluster]
+# }
 
-# Inclusão de permissões globais em todos os namespaces
-resource "kubectl_manifest" "csi_secret_clusterrole" {
-  yaml_body = <<-EOT
-apiVersion: rbac.authorization.k8s.io/v1
-kind: ClusterRole
-metadata:
-  name: secrets-store-csi-driver-global-clusterrole
-rules:
-- apiGroups: [""]
-  resources: ["secrets"]
-  verbs: ["create", "get", "update", "delete"]
-EOT
+# # Inclusão de permissões globais em todos os namespaces
+# resource "kubectl_manifest" "csi_secret_clusterrole" {
+#   yaml_body = <<-EOT
+# apiVersion: rbac.authorization.k8s.io/v1
+# kind: ClusterRole
+# metadata:
+#   name: secrets-store-csi-driver-global-clusterrole
+# rules:
+# - apiGroups: [""]
+#   resources: ["secrets"]
+#   verbs: ["create", "get", "update", "delete"]
+# EOT
 
-  depends_on = [module.eks_cluster]
-}
+#   depends_on = [module.eks_cluster]
+# }
 
-resource "kubectl_manifest" "csi_secret_clusterrolebinding" {
-  yaml_body = <<-EOT
-apiVersion: rbac.authorization.k8s.io/v1
-kind: ClusterRoleBinding
-metadata:
-  name: secrets-store-csi-driver-global-clusterrolebinding
-subjects:
-- kind: ServiceAccount
-  name: secrets-store-csi-driver
-  namespace: kube-system
-roleRef:
-  kind: ClusterRole
-  name: secrets-store-csi-driver-global-clusterrole
-  apiGroup: rbac.authorization.k8s.io
-EOT
+# resource "kubectl_manifest" "csi_secret_clusterrolebinding" {
+#   yaml_body = <<-EOT
+# apiVersion: rbac.authorization.k8s.io/v1
+# kind: ClusterRoleBinding
+# metadata:
+#   name: secrets-store-csi-driver-global-clusterrolebinding
+# subjects:
+# - kind: ServiceAccount
+#   name: secrets-store-csi-driver
+#   namespace: kube-system
+# roleRef:
+#   kind: ClusterRole
+#   name: secrets-store-csi-driver-global-clusterrole
+#   apiGroup: rbac.authorization.k8s.io
+# EOT
 
-  depends_on = [module.eks_cluster]
-}
+#   depends_on = [module.eks_cluster]
+# }
--- modules/secrets/secret.tf
+++ modules/secrets/secret.tf
@@ -1,55 +1,106 @@
-resource "aws_iam_policy" "policy_secret" {
-  name        = "${var.project_name}-secret-policy"
-  description = "Policy to allow reading API tokens from Secrets Manager and accessing S3"
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect   = "Allow",
-        Action   = "secretsmanager:*",
-        Resource = "*"
-      },
-      {
-        Effect   = "Allow",
-        Action   = [
-          "s3:ListBucket",
-          "s3:GetObject",
-          "s3:PutObject",
-          "s3:DeleteObject"
-        ],
-        Resource = [
-          "arn:aws:s3:::img-app0001",
-          "arn:aws:s3:::img-app0001/*"
-        ]
-      }
+data "aws_iam_policy_document" "eso" {
+  statement {
+    actions = [
+      "secretsmanager:GetSecretValue"
     ]
-  })
+    resources = [
+      "arn:aws:secretsmanager:us-east-1:${var.aws_account_id}:secret:*"
+    ]
+  }
 }
 
-resource "aws_iam_role" "role_secret" {
-  name = "${var.project_name}-secret-role"
+resource "aws_iam_policy" "eso_policy" {
+  name   = "eso-secretsmanager-policy"
+  policy = data.aws_iam_policy_document.eso.json
+}
+
+resource "aws_iam_role" "eso_role" {
+  name = "eso-service-account-role"
 
   assume_role_policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
-      {
-        "Effect" : "Allow",
-        "Principal" : {
-          "Federated" : "arn:aws:iam::${var.aws_account_id}:oidc-provider/${var.oidc_issuer_url}"
-        },
-        "Action" : "sts:AssumeRoleWithWebIdentity",
-        "Condition" : {
-          "StringEquals" : {
-            "${var.oidc_issuer_url}:aud": "sts.amazonaws.com"
-          }
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Federated = "arn:aws:iam::${var.aws_account_id}:oidc-provider/${var.oidc_issuer_url}"
+      },
+      Action = "sts:AssumeRoleWithWebIdentity",
+      Condition = {
+        StringEquals = {
+          "${var.oidc_issuer_url}:sub" = "system:serviceaccount:external-secrets:external-secrets"
         }
       }
-    ]
+    }]
   })
 }
 
-resource "aws_iam_role_policy_attachment" "attach_policy_secret" {
-  role       = aws_iam_role.role_secret.name
-  policy_arn = aws_iam_policy.policy_secret.arn
+resource "aws_iam_role_policy_attachment" "eso_attach" {
+  role       = aws_iam_role.eso_role.name
+  policy_arn = aws_iam_policy.eso_policy.arn
 }
+
+resource "kubernetes_service_account" "eso_sa" {
+  metadata {
+    name      = "external-secrets"
+    namespace = "external-secrets"
+    annotations = {
+      "eks.amazonaws.com/role-arn" = aws_iam_role.eso_role.arn
+    }
+  }
+}
+
+# resource "aws_iam_policy" "policy_secret" {
+#   name        = "${var.project_name}-secret-policy"
+#   description = "Policy to allow reading API tokens from Secrets Manager and accessing S3"
+
+#   policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [
+#       {
+#         Effect   = "Allow",
+#         Action   = "secretsmanager:*",
+#         Resource = "*"
+#       },
+#       {
+#         Effect   = "Allow",
+#         Action   = [
+#           "s3:ListBucket",
+#           "s3:GetObject",
+#           "s3:PutObject",
+#           "s3:DeleteObject"
+#         ],
+#         Resource = [
+#           "arn:aws:s3:::img-app0001",
+#           "arn:aws:s3:::img-app0001/*"
+#         ]
+#       }
+#     ]
+#   })
+# }
+
+# resource "aws_iam_role" "role_secret" {
+#   name = "${var.project_name}-secret-role"
+
+#   assume_role_policy = jsonencode({
+#     "Version" : "2012-10-17",
+#     "Statement" : [
+#       {
+#         "Effect" : "Allow",
+#         "Principal" : {
+#           "Federated" : "arn:aws:iam::${var.aws_account_id}:oidc-provider/${var.oidc_issuer_url}"
+#         },
+#         "Action" : "sts:AssumeRoleWithWebIdentity",
+#         "Condition" : {
+#           "StringEquals" : {
+#             "${var.oidc_issuer_url}:aud": "sts.amazonaws.com"
+#           }
+#         }
+#       }
+#     ]
+#   })
+# }
+
+# resource "aws_iam_role_policy_attachment" "attach_policy_secret" {
+#   role       = aws_iam_role.role_secret.name
+#   policy_arn = aws_iam_policy.policy_secret.arn
+# }
